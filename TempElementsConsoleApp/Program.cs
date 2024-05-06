3c396777b6e40954c2f6642d7e0f66d1cf6a0b11
Kacper Kułakowski <kacper.kulakowski@microsoft.wsei.edu.pl>
2024-05-06 23:46:43 +0200

    Add project files.

---
+++ TempElementsConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TempElementsLib;
+using System.IO;
+
+namespace TempElementsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tworzenie pliku przy użyciu \"standardowych\" komend");
+            TempFile tempFile = new TempFile();                                             // tworzenie pliku w domyslnej dla systemu/użytkownika lokalizacji i o losowej nazwie
+            Console.WriteLine(tempFile.FilePath);                                           // ścieżka dostępu do pliku
+            Console.WriteLine(tempFile.IsDestroyed);                                        // false
+            tempFile.Dispose();                                                             // usuwanie pliku
+            Console.WriteLine(tempFile.IsDestroyed);                                        // true
+            Console.WriteLine("Próba zapisu do pliku po jego zamknięciu");
+            try
+            {
+                tempFile.AddText("Hello World!");                                           // próba zapisu do plik
+            }
+            catch (Exception ex)                                                            // przechwytywanie wyjątku
+            {
+                Console.WriteLine(ex.Message);                                              // wyświetlenie komunikatu o błędzie
+            }
+            Console.WriteLine("\nTworzenie pliku przy użyciu bloku using");
+            using (TempFile tempFile2 = new TempFile())                                     // tworzenie katalogu w domyslnej dla systemu/użytkownika lokalizacji
+            {
+                Console.WriteLine(tempFile2.FilePath);                                      // ścieżka dostępu do pliku
+                Console.WriteLine(tempFile2.IsDestroyed);                                   // false
+                                                                                            // usuwanie pliku
+
+            }
+            Console.WriteLine("\nTworzenie pliku przy użyciu bloku try-catch-finally");
+            TempFile tempFile3 = new TempFile();                                            // tworzenie pliku w domyslnej dla systemu/użytkownika lokalizacji i o losowej nazwie
+            try                                                                             // blok try-catch
+            {
+                Console.WriteLine(tempFile3.FilePath);                                      // ścieżka dostępu do pliku
+                Console.WriteLine(tempFile3.IsDestroyed);                                   // false
+                tempFile3.AddText("Hello World!");                                          // zapis do pliku
+                Console.WriteLine(tempFile3.IsDestroyed);                                   // false
+            }
+            catch (Exception ex)                                                            // przechwytywanie wyjątku
+            {
+                Console.WriteLine(ex.Message);                                              // wyświetlenie komunikatu o błędzie
+            }
+            finally                                                                         // blok finally
+            {
+                tempFile3.Dispose();                                                        // usuwanie pliku
+                Console.WriteLine(tempFile3.IsDestroyed);                                   // true
+            }
+
+        }
+    }
+}
---
+++ TempElementsLib/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+
+namespace TempElementsLib
+{
+    public class TempFile : ITempFile
+    {
+
+        public bool IsDestroyed { get; private set; }
+        public string FilePath => fileInfo.FullName;
+
+        public readonly FileStream fileStream;
+        public readonly FileInfo fileInfo;
+
+        public TempFile()
+        {
+            string tempPath = Path.GetTempFileName();
+            fileStream = new FileStream(tempPath, FileMode.Open, FileAccess.ReadWrite);
+            fileInfo = new FileInfo(tempPath);
+        }
+
+        ~TempFile()
+        {
+            Dispose(false);
+        }
+
+        public void AddText(string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fileStream.Write(info, 0, info.Length);
+            fileStream.Flush();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDestroyed)
+            {
+                if (disposing)
+                {
+                    fileStream?.Close();
+                    fileInfo?.Delete();
+                }
+                IsDestroyed = true;
+            }
+        }
+    }
+
+}
+
